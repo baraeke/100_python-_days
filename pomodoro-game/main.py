b913cdbdd07ee51aa9e749c0dcceffde70b3575c
Destiny Diekedie <indianedindians.com>
2025-04-10 21:30:37 +0000

    count down mechanism completed

--- pomodoro-game/main.py
+++ pomodoro-game/main.py
@@ -13,7 +13,12 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    canvas.itemconfig(timer_text, text = count)
+    if count > 0:
+        window.after(1000, count_down, count -1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -26,10 +31,12 @@ timer_label.grid(row= 0, column=1)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness= 0)
 tomato_img = PhotoImage(file= "tomato.png")
 canvas.create_image(100, 112, image = tomato_img)
-canvas.create_text(100, 130, text="00:00", fill= "white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill= "white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row= 1, column= 1)
 
-start_btn = Button(text= "Start",highlightthickness= 0, command= print)
+count_down(5)
+
+start_btn = Button(text= "Start",highlightthickness= 0, command= start)
 start_btn.grid(row= 2, column= 0)
 
 reset_btn = Button(text= "Reset", highlightthickness=0, command= print)
