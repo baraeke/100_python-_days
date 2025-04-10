2b1afdf603783876098fcecb5c231c406df8eb4e
Destiny Diekedie <indianedindians.com>
2025-04-10 15:55:16 +0000

    UI completed

--- pomodoro-game/main.py
+++ pomodoro-game/main.py
@@ -20,14 +20,22 @@ window = Tk()
 window.title("Pamadora")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-
+timer_label = Label(text= "Timer", font= (FONT_NAME, 35, "normal"), fg= GREEN, bg= YELLOW)
+timer_label.grid(row= 0, column=1)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness= 0)
 tomato_img = PhotoImage(file= "tomato.png")
 canvas.create_image(100, 112, image = tomato_img)
 canvas.create_text(100, 130, text="00:00", fill= "white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(row= 1, column= 1)
 
+start_btn = Button(text= "Start", command= print)
+start_btn.grid(row= 2, column= 0)
 
+reset_btn = Button(text= "Reset", command= print)
+reset_btn.grid(row=2, column=2)
+
+check = Label(text= "✓", fg= GREEN, bg= YELLOW, font= (FONT_NAME, 35, "bold"))
+check.grid(row=3, column= 1)
 
 window.mainloop()
